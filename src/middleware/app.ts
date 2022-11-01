bb2645ab40e22b6503f531056a1d0c5b7af8fc38
Rafa Alastuey <rafa@rab-devs.com>
2022-11-01 23:04:26 +0100

    Minor change

---
+++ src/middleware/app.ts
@@ -0,0 +1,3 @@
+import { Context } from '@nuxt/types'
+
+export default function (ctx: Context) {}
---
+++ src/pages/index.vue
@@ -0,0 +1,28 @@
+<script lang="ts">
+
+import Vue from 'vue';
+
+export default Vue.extend({
+    head() {
+        return {
+            meta: [
+                {
+                    hid: 'description', name: 'description',
+                    content: this.$i18n.t('pages.index.meta.description').toString(),
+                },
+                {
+                    hid: 'og:description', property: 'og:description',
+                    content: this.$i18n.t('pages.index.meta.description').toString(),
+                },
+                {
+                    hid: 'twitter:description', name: 'twitter:description',
+                    content: this.$i18n.t('pages.index.meta.description').toString(),
+                },
+            ],
+        }
+    },
+})
+</script>
+
+<template>
+</template>
