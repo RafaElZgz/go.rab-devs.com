cb5b3873009d080369e066ba127088f824bb58e8
Rafa Alastuey <rafa@rab-devs.com>
2022-11-02 11:01:02 +0100

    App middleware

--- src/middleware/app.ts
+++ src/middleware/app.ts
@@ -1,34 +1,35 @@
 import { Context } from '@nuxt/types';
 
+interface Visit {
+    id: string,
+    browser: string | undefined,
+    browserLanguage: string | undefined,
+    browserVersion: string | undefined,
+    city: string | undefined,
+    country: string | undefined,
+    ipAddress: string,
+    isp: string | undefined,
+    language: string,
+    os: string | undefined,
+    osVersion: string | undefined,
+    region: string | undefined,
+}
+
 interface ShortenedLink {
+    id: string,
     user: Object,
     name: string,
     slug: string,
     url: string,
     visits: {
         count: number,
-        list: [
-            {
-                id: string,
-                browser: string,
-                browserLanguage: string,
-                browserVersion: string,
-                city: string,
-                country: string,
-                ipAddress: string,
-                isp: string,
-                language: string,
-                os: string,
-                osVersion: string,
-                region: string,
-            }
-        ],
+        list: Visit[],
     },
 }
 
 export default async function (ctx: Context) {
 
-    if (ctx.route.path.startsWith('/app')) {
+    if (ctx.route.path === '/') {
 
         ctx.redirect(ctx.app.localePath('app'));
 
@@ -40,6 +41,35 @@ export default async function (ctx: Context) {
                 slug: ctx.route.params.slug,
             }
         ).catch(() => { ctx.error({ statusCode: 404 }); }) as ShortenedLink;
+
+        const visit: Visit = {
+            id: '',
+            browser: '',
+            browserLanguage: '',
+            browserVersion: '',
+            city: '',
+            country: '',    
+            ipAddress: '',
+            isp: '',
+            language: ctx.i18n.locale,
+            os: '',
+            osVersion: '',
+            region: '',
+        };
+
+        const visits_list: Visit[] = url.visits.list;
+        visits_list.push(visit);
+
+        const new_visits = {
+            count: url.visits.count + 1,
+            list: visits_list,
+        };
+
+        await ctx.app.$strapi.update('shortened-links', url.id,
+            {
+                visits: new_visits,
+            }
+        ) as ShortenedLink;
 
         ctx.redirect(url.url);
     }
